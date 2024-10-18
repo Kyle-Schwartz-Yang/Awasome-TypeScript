6c46f2413691194cedc8949bb7b9257605a8282d
schwartz <gtfsolo@gmail.com>
2024-10-18 14:23:58 +0300

    basic types assertion

---
+++ Awesome/types/assertions__as/Assertions.ts
@@ -0,0 +1,25 @@
+interface Person {
+  name: string,
+  surname: string,
+  age: number,
+  isAdmin: boolean,
+}
+
+const asserionObj: Person = {
+  name: 'Jack',
+  surname: 'Simpon',
+  age: 22,
+  isAdmin: true,
+}
+
+// keyof указывет на то что key является один из ключей interface Person
+
+for (let key in asserionObj) {
+  console.log(asserionObj[key as keyof Person])
+}
+
+// Использования Второй синтаксис type assertion
+
+for (let key in asserionObj) {
+  console.log(asserionObj[<keyof Person>key])
+}
