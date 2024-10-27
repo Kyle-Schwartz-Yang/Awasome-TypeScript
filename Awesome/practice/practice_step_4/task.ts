c4294eb2b2bf5a47c706f09fc1cad14623fd2ac8
schwartz <gtfsolo@gmail.com>
2024-10-27 23:54:03 +0200

    add operator Assertions more information

--- Awesome/practice/practice_step_4/task.ts
+++ Awesome/practice/practice_step_4/task.ts
@@ -11,32 +11,35 @@ export = {}
 // subtitles - необязательное поле типа строка
 // marks - необязательное поле неизвестного типа
 
-function playMedia(
-  { name, type, format, subtitles, marks }: интерфейс = {
-    name: "example",
-    type: один из типов,
-    format: один из форматов,
+/*
+  function playMedia(
+    { name, type, format, subtitles, marks }: интерфейс = {
+      name: "example",
+      type: один из типов,
+      format: один из форматов,
+    }
+  ): string {
+    let marksLog;
+
+    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+    // Если это строка, то просто поместить её в marksLog
+    // Если что-то другое - то marksLog = "Unsupported type of marks"
+    // Не допускайте any!
+
+    console.log(`Media ${name}${format} is ${type}
+      Marks: ${marksLog}
+      Subtitles: ${subtitles ?? "none"}`);
+    // помните что это за оператор ??
+
+    return "Media started";
   }
-): string {
-  let marksLog;
+ */
 
-  // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
-  // Если это строка, то просто поместить её в marksLog
-  // Если что-то другое - то marksLog = "Unsupported type of marks"
-  // Не допускайте any!
-
-  console.log(`Media ${name}${format} is ${type}
-    Marks: ${marksLog}
-    Subtitles: ${subtitles ?? "none"}`);
-  // помните что это за оператор ??
-
-  return "Media started";
-}
-
-playMedia({
-  name: "WoW",
-  format: один из форматов,
-  type: один из типов,
-  subtitles: "hmhmhm hmhmhm doh",
-  marks: ["4:30", "5:40"],
-});
+// ---------------------------------------------------------------
+// playMedia({
+//   name: "WoW",
+//   format: один из форматов,
+//   type: один из типов,
+//   subtitles: "hmhmhm hmhmhm doh",
+//   marks: ["4:30", "5:40"],
+// });
