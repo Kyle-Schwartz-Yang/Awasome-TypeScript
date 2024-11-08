9fbe610459628ba766a6a544f6a058bf19001073
schwartz <gtfsolo@gmail.com>
2024-11-08 20:40:24 +0200

    finish practice 5

--- Awesome/practice/responce_step_5/check.ts
+++ Awesome/practice/responce_step_5/check.ts
@@ -2,10 +2,7 @@
 
 export {}
 
-
-
 // Можно и не создавать перечисление, но вдруг в будущем статусов будет больше?
-
 
 interface iAnimals {
 
@@ -14,7 +11,6 @@ interface iAnimals {
 	sterilized?: string;
 
 }
-
 
 interface iSuccess {
   status: 'available',
@@ -29,7 +25,6 @@ interface iSuccess {
 
 }
 
-
 interface iError {
   status: 'not available',
 
@@ -37,11 +32,7 @@ interface iError {
       message: string,
       nextUpdateIn: Date
   }
-
 }
-
-
-
 
 // function isAvailable(res: Res): res is iSuccess {
 // 	if (res.status === 'available') {
@@ -51,15 +42,14 @@ interface iError {
 // 	}
 // }
 
-
 /*
 - Как называется запись iSuccess['data'] ?
 - Такая запись называется доступ к типу по индексу (или Indexed Access Type)
 - Этот синтаксис позволяет получать тип конкретного свойства интерфейса или типа, обращаясь к нему по имени через квадратные скобки
-
 */
 
 type Res = iSuccess | iError;
+
 
 function checkAnimalData(animal: Res): iSuccess['data'] | string {
 
@@ -70,3 +60,4 @@ function checkAnimalData(animal: Res): iSuccess['data'] | string {
 	}
 
 }
+
--- Awesome/types/__theme/Narrowing-typeGuards/typePredicate/predicate.ts
+++ Awesome/types/__theme/Narrowing-typeGuards/typePredicate/predicate.ts
@@ -1,6 +1,9 @@
 export {}
 
 
+
+
+
 /* 
  Пользовательнский type guards.
  По сути проверяет число это или нет.
@@ -22,6 +25,18 @@ interface Ship {
   sail: string;
 }
 
+
+
+/*
+
+1. Type Predicate — это выражение pet is Car, которое указывает TypeScript, что если функция возвращает true, то переменная pet имеет тип Car.
+
+2. Type Guard — функция isShip, использующая проверку на наличие свойства ("wheels" in pet) для уточнения типа pet. Если это свойство есть, TypeScript понимает, что объект скорее всего типа Car, поскольку Ship такого свойства не имеет.
+
+*/
+
+// ! function isShip : ЭТО ПО СУТИ ПОЛЬЗОВАТЕЛЬНСКИЙ TYPE GUADRS 
+// #  pet is Car : ЭТО ПО СУТИ Type Predicate
 function isShip(pet: Car | Ship): pet is Car {
   return "wheels" in pet // Проверка на наличие свойства sail
 }
